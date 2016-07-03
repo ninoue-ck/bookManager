443a34c053c68f567263740a56d0770550cedd81
ninoue-ck <n.inoue@caraquri.com>
2016-07-03 23:06:04 +0900

    テーブルビューの「さらに読みこむ」フッターの追加

---
+++ bookManager/SettingViewController.h
@@ -0,0 +1,14 @@
+//
+//  SettingViewController.h
+//  bookManager
+//
+//  Created by inouenaoto on 2016/07/03.
+//  Copyright © 2016年 inouenaoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SettingViewController : UIViewController
+
+@end
